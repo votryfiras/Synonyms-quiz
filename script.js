376593c2ba9d1249612938cfd5d30abc3b15b069
Faroos Ayman <firas.ayman@yahoo.com>
2023-05-20 16:52:37 +0400

    Dictionary and style changes
    that only.

--- script.js
+++ script.js
@@ -1,10 +1,51 @@
-const WORDS = [{
-  word: 'buy',
-  synonyms: ['purchase'],
-  antonyms: ['sell'],
-  languageLevel: 'A1',
-  sentences: ['When buying and selling in the marketplace they scrutinize and carefully choose every single coin.', 'As was mentioned earlier, newcomers who buy supplemental insurance must bear a waiting period, which is expected to discourage moves.', 'In this new luxury market paradigm a great number of people can buy luxury goods.']
-}]
+const WORDS = [
+  {
+    word: "buy",
+    def: ["to get something by giving money for it"],
+    syns: ["purchase", "acquire"],
+    languageLevel: "A1",
+    examples: ['Eventually she had saved enough money to buy a small car.', 'He bought his mother some flowers/He bought some flowers for his mother.', 'There are more people buying at this time of the year so prices are high.', 'The company was set up to buy and sell shares on behalf of investors.', 'I bought my camera from a friend of mine.']
+  },
+  {
+    word: "basin",
+    def: ["an open, round container shaped like a bowl with sloping sides, used for holding food or liquid", "a large, open bowl, or the amount such a container will hold"],
+    syns: ["bowl", "dish", "pan", "pot", 'container', 'receptacle', "valley", "hollow", "gully", "gorge", "ravine", "bed", "channel", "dip", "depression", "concavity", "trough"],
+    languageLevel: "B1",
+    examples: ['Run some water into the basin and wash your hands and face properly.', 'When you have broken the eggs into a basin, whisk them together lightly with a fork.', 'The basin in the upstairs bathroom has gold taps! …eaning fluids under the basin in the back toilet.', 'In the auction there is a rather nice antique porcelain basin and jug.', 'I left the napkins soaking in a basin.']
+  },
+  {
+    word: "carbon footprint",
+    def: ["a measure of the amount of carbon dioxide released into the atmosphere as a result of the activities of a particular individual, organization, or community."],
+    syns: ["pollution", "impurity"],
+    languageLevel: "B2",
+    examples: ['The four main areas that determine your carbon foo…tural gas usage, car mileage, and airplane trips.', 'We have partnered with nearby farms, hoping to reduce the carbon footprint of our delivery trucks.'],
+  },
+  {
+    word: "conserve",
+    def: ["protect (something, especially something of environmental or cultural importance) from harm or destruction."],
+    syns: ["perserve"],
+    languageLevel: "",
+    examples: ['To conserve electricity, we are cutting down on our heating.', 'The nationalists are very eager to conserve their customs and language.', "I'm not being lazy - I'm just conserving my energy/strength for later."],
+  },
+  {
+    word: "consume",
+    def: ["to use fuel, energy, time, or a product, especially in large amounts", "to eat or drink something"],
+    syns: ["eat", "devour", "ingest", "swallow", "absorb"],
+    languageLevel: "C1",
+    examples: ["Our high living standards cause our current population to consume 25 percent of the world's oil.", 'Most of their manufactured products are consumed domestically.', 'The software consumes huge amounts of internet bandwidth.', "He consumes huge amounts of bread with every meal."]
+  },
+]
+
+function getSentences(str) {
+  let sentence = ''
+  const exmpls = []
+  for (const i in str) {
+    const letter = str[i]
+    if (letter !== '.') { sentence = sentence + letter }
+    else { exmpls.push(sentence.trim() + '.'); sentence = "" }
+  }
+  console.log(exmpls)
+}
 
 const choices = document.querySelectorAll('.choice')
 const modeSelect = document.querySelector('.mode-select')
